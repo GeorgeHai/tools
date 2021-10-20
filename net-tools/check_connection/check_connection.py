d2d141f2197687b04b5f988b23adee57b54a2c5c
GeorgeHai <91995566+GeorgeHai@users.noreply.github.com>
2021-10-20 22:19:22 +0200

    new documentation, new functionality
    added documentation of code. If additional informations are needed just contact me.
    
    The results of the ping-request are now written into a file, if needed.

--- net-tools/check_connection/check_connection.py
+++ net-tools/check_connection/check_connection.py
@@ -3,13 +3,27 @@ import requests
 import conn_tools
 from datetime import datetime
 
+"""
+current_time() returns based on datetime the current time and returns it for further usage
+"""
+
 
 def current_time():
     now = datetime.now()
     return now.strftime("%D %H:%M:%S")
 
 
-def connection_check(url, timeout, ip_table):
+"""
+The Method connection_check is used to check your server-connection in the background.
+The the given url isn't available, it starts to check the ip-addresses from the provided ip_table.
+The results are written to a specified file.
+
+After the connection-check the method takes a break for 10 seconds and restarts. This can be deactivated by 
+uncommenting the line 31. 
+"""
+
+
+def connection_check(url, timeout, ip_table, log_file=True, path='./'):
     flag = True
     while (flag):
         try:
@@ -18,12 +32,20 @@ def connection_check(url, timeout, ip_table):
         except (requests.ConnectionError, requests.Timeout):
             for ip in ip_table:
                 print('\n {} Check ip-adress: {}'.format(current_time(), ip))
-                conn_tools.ping(ip)
-            flag = False
-            print("Programm stops now")
+                conn_tools.ping(ip, log_file=log_file, path=path)
+            # flag = False          # you can deactivate reactivation in this line
+            if (flag == False):
+                print("Programm stops in 10 sec.")
         time.sleep(10)
 
 
+"""
+The ip_table contains the provided ip-addresses you would like to check, if the connection is timed-out.
+"""
 if __name__ == '__main__':
     ip_table = '192.168.178.1', '192.168.178.45'
-    connection_check(url='http://google.com', timeout=5, ip_table=ip_table)
+    connection_check(url='http://google.com',
+                     timeout=5,
+                     ip_table=ip_table,
+                     log_file=False,
+                     path='./logs/')
--- net-tools/check_connection/conn_tools.py
+++ net-tools/check_connection/conn_tools.py
@@ -1,8 +1,20 @@
 import platform
 import subprocess
 
-
-def ping(host):
+"""
+The method ping uses provided data to create a subprocess-call.
+The results can be written to a file.
+"""
+def ping(host, log_file=True, path='./'):
     param = '-n' if platform.system().lower() == 'windows' else '-c'
     command = ['ping', param, '5', host]
-    return subprocess.call(command) == 0
+    if log_file:
+        result_file = '{}{}.txt'.format(path, host)
+        f = open(result_file, 'w')
+        return subprocess.call(command, stdout=f) == 0
+    else:
+        return subprocess.call(command) == 0
+
+
+if __name__ == '__main__':
+    ping('192.168.1.1', log_file=True, path='./')
